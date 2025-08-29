57da1066c9047c6d63753b2df507ccf5ab963943
qilin <sql1211@1qq.com>
2025-08-29 15:46:36 +0800

    init

---
+++ src/test/java/com/example/screencorexserver/ScreencorexServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.screencorexserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreencorexServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
